78a87830cc477003695b4ff96207b49d63b8bec3
aki <lala.akira@gmail.com>
2021-05-16 18:42:00 +0900

    remove dist

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-declare type Component = React.FunctionComponent<any> | React.ComponentClass<any>;
-export declare const rewrap: (name: string, component: Component, hasChildren?: boolean) => void;
-export {};
