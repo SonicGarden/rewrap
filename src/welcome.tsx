9a08e1a382ca0a4350fdbc0f1b917c237d33398d
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2022-07-29 15:32:44 +0900

    fix: restore children after unmount react component

--- src/welcome.tsx
+++ src/welcome.tsx
@@ -3,3 +3,7 @@ import React from 'react';
 export function Welcome({ name }: { name: string }) {
   return <h1>Hello, {name}</h1>;
 }
+
+export function WelcomeWithChildren({ children }: { children: React.ReactNode }) {
+  return <h1>Hello, {children}</h1>;
+}
